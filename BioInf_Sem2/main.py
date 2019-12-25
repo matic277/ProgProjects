820e7d43b3fe8a0a58d3a04008ad0cfd11c06094
matic277 <matic277@gmail.com>
2019-12-26 00:11:05 +0100

    bioinf added reading precomputed matrices and graph drawing function

--- BioInf_Sem2/main.py
+++ BioInf_Sem2/main.py
@@ -1,67 +1,77 @@
 import gene
+import dndgram
 #import sklearn
 
+# reading and creating matrix from scratch
+# def readFile():
+#     filepath = 'genes.txt'
+#
+#     with open(filepath) as f:
+#         lines = f.readlines()
+#     lines = [x.strip() for x in lines]
+#
+#     geneClasses = []
+#
+#     for i in range(len(lines)):
+#         l = lines[i]
+#         if "\t" not in l:
+#             batch = []
+#             for j in range(7):
+#                 batch.append(lines[i + j + 1])
+#             #print(l)
+#             #print(batch)
+#             geneClasses.append(gene.GeneClass(l, batch))
+#
+#     return geneClasses
+#
+# geneClasses = readFile()
+#
+# dm = []
+# for g in geneClasses:
+#     m = g.getAlignmentMatrix()
+#     # create matrix of [id, distance_matrix]
+#     dm.append([g.id, m])
+#     print("id: ", g.id)
+#     print("dm: ")
+#     for l in m:
+#         print("  ", l)
+#     print("")
+#
+# print(" -> DONE.")
+
+genemtx = []
+
+# reading precomputed matrices
 def readFile():
-    filepath = 'genes.txt'
+    filepath = "precomp_md_custom_jc.txt"
 
     with open(filepath) as f:
         lines = f.readlines()
-    lines = [x.strip() for x in lines]
 
-    geneClasses = []
+    for i in range(0, len(lines), 8):
+        id = lines[i].strip("\n")
+        m = []
+        for j in range(1, 8):
+            #print(i+j)
+            vals = lines[i+j]
+            vals = vals.replace("[", "").replace("]", "").replace("\n", "").replace(",", "").replace("'", "")
+            tokens = vals.split(" ")
+            subm = []
+            for t in tokens:
+                subm.append(float(t))
+            m.append(subm)
+        genemtx.append([id, m])
 
-    for i in range(len(lines)):
-        l = lines[i]
-        if "\t" not in l:
-            batch = []
-            for j in range(7):
-                batch.append(lines[i + j + 1])
-            #print(l)
-            #print(batch)
-            geneClasses.append(gene.GeneClass(l, batch))
-
-    return geneClasses
-
-geneClasses = readFile()
-# print("ALL GENES READ:")
-# for gc in geneClasses:
-#     gc.printAll()
-
-# r = geneClasses[0].getSequenceById("9913")
-# print(r.seq)
-
-a = "A"
-if a == "-":
-    print("yes")
-else:
-    print("no")
-
-dm = []
-for g in geneClasses:
-    m = g.getAlignmentMatrix()
-    # create matrix of [id, distance_matrix]
-    dm.append([g.id, m])
-    print("id: ", g.id)
-    print("dm: ")
-    for l in m:
-        print("  ", l)
-    print("")
-
-# dm = [[['123'], [[1, 2],[3, 4]]], [['abc'], [[6, 7],[8, 9]]]]
-#
-# for m in dm:
-#     print("id: ", m[0])
-#     print("dm: ")
-#     for l in m[1]:
-#         print("  ", l)
-#     print("\n")
+readFile()
 
 
-print(" -> DONE.")
-# for l in r2:
-#     print(l)
+dndgram.plotAll(genemtx)
 
-# AglomerateCluster from scipy?
+print("done")
+
+
+
+
 
 
 
